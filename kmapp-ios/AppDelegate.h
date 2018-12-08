39d9c4796483fbe6c1c2fa8753e070c8fb6fd79e
lovecross <lovecrossyoutt@hotmail.com>
2018-12-08 09:50:51 +0800

    first commit

---
+++ kmapp-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kmapp-ios
+//
+//  Created by 朱理哲 on 2018/12/8.
+//  Copyright © 2018 com.huibei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
